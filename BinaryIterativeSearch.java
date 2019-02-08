b18ada2941c641e129abbc477589a08d38feffba
djbarajas <35737585+djbarajas@users.noreply.github.com>
2019-02-08 14:29:23 -0800

    Worked on binary iterative search

--- BinaryIterativeSearch.java
+++ BinaryIterativeSearch.java
@@ -6,6 +6,22 @@ public class BinaryIterativeSearch implements Practice03Search{
 
     @Override
     public int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+        int middle = end/2;
+        while(start < end){
+            middle = (start+end)/2;
+            if(arr[middle] > target){
+                end = middle-1;
+            }
+            else if(arr[middle] < target){
+                start = middle+1;
+            }
+            else if(arr[middle] == target){
+                return middle;
+            }
+
+        }
         return 0;
     }
 }
